e3f722cf5f82a8dc5db5fc6dd72327382c5ff78a
leila <143362178+1e1104k4@users.noreply.github.com>
2024-04-03 17:18:52 -0500

    init pos on map

--- BasketList/ContentView.swift
+++ BasketList/ContentView.swift
@@ -5,17 +5,26 @@
 //  Created by Leila on 4/3/24.
 //
 
+import MapKit
 import SwiftUI
 
 struct ContentView: View {
+    let startPosition = MapCameraPosition.region(
+        MKCoordinateRegion(
+            center: CLLocationCoordinate2D(latitude: 21.3, longitude: -157.5),
+            span: MKCoordinateSpan(latitudeDelta: 7, longitudeDelta: 7)
+        )
+    )
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        MapReader { proxy in
+            Map(initialPosition: startPosition)
+                .onTapGesture { position in
+                    if let coordinate = proxy.convert(position, from: .local) {
+                        print("Tapped at \(coordinate)")
+                    }
+                }
         }
-        .padding()
     }
 }
 
