18f78b41632cfec355420eec914c7f2253bd6232
leila <143362178+1e1104k4@users.noreply.github.com>
2024-04-03 15:37:17 -0500

    Initial Commit

---
+++ BasketList/BasketListApp.swift
@@ -0,0 +1,17 @@
+//
+//  BasketListApp.swift
+//  BasketList
+//
+//  Created by Leila on 4/3/24.
+//
+
+import SwiftUI
+
+@main
+struct BasketListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
