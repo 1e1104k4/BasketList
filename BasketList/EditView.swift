c68963fe488266a5aed856b36d3e4ce4f34141f3
leila <143362178+1e1104k4@users.noreply.github.com>
2024-04-05 13:45:48 -0500

    Edit View

---
+++ BasketList/EditView.swift
@@ -0,0 +1,44 @@
+//
+//  EditView.swift
+//  BasketList
+//
+//  Created by Leila on 4/4/24.
+//
+
+import SwiftUI
+
+struct EditView: View {
+    @Environment(\.dismiss) var dismiss
+    var location: Location
+    
+    @State private var name: String
+    @State private var description: String
+    
+    var body: some View {
+        NavigationStack {
+            Form {
+                Section {
+                    TextField("Place name", text: $name)
+                    TextField("Description", text: $description)
+                }
+            }
+            .navigationTitle("Place details")
+            .toolbar {
+                Button("Save") {
+                    dismiss()
+                }
+            }
+        }
+    }
+    
+    init(location: Location) {
+        self.location = location
+        
+        _name = State(initialValue: location.name)
+        _description = State(initialValue: location.description)
+    }
+}
+
+#Preview {
+    EditView(location: .example)
+}
