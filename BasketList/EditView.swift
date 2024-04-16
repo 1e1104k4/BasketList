d813cdbd3eddf628cacd2826371fd480694470ad
leila <143362178+1e1104k4@users.noreply.github.com>
2024-04-16 16:12:10 -0500

    props moved to EditView-ViewModel

--- BasketList/EditView.swift
+++ BasketList/EditView.swift
@@ -8,19 +8,9 @@
 import SwiftUI
 
 struct EditView: View {
-    enum LoadingState {
-        case loading, loaded, failed
-    }
-    
     @Environment(\.dismiss) var dismiss
-    var location: Location
     
-    @State private var name: String
-    @State private var description: String
     var onSave: (Location) -> Void
-    
-    @State private var loadingState = LoadingState.loading
-    @State private var pages = [Page]()
     
     var body: some View {
         NavigationStack {
@@ -72,24 +62,6 @@ struct EditView: View {
         
         _name = State(initialValue: location.name)
         _description = State(initialValue: location.description)
-    }
-    
-    func fetchNearbyPlaces() async {
-        let urlString = "https://en.wikipedia.org/w/api.php?ggscoord=\(location.latitude)%7C\(location.longitude)&action=query&prop=coordinates%7Cpageimages%7Cpageterms&colimit=50&piprop=thumbnail&pithumbsize=500&pilimit=50&wbptterms=description&generator=geosearch&ggsradius=10000&ggslimit=50&format=json"
-        
-        guard let url = URL(string: urlString) else {
-            print("Bad URL: \(urlString)")
-            return
-        }
-        
-        do {
-            let (data, _) = try await URLSession.shared.data(from: url) // fetch
-            let items = try JSONDecoder().decode(Result.self, from: data) // decode
-            pages = items.query.pages.values.sorted()
-            loadingState = .loaded
-        } catch {
-            loadingState = .failed
-        }
     }
 }
 
