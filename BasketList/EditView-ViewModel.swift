beb07e6f734b2283ce773dcc00f4646b2a23b7de
leila <143362178+1e1104k4@users.noreply.github.com>
2024-04-16 16:15:56 -0500

    init for edit viewmodel

--- BasketList/EditView-ViewModel.swift
+++ BasketList/EditView-ViewModel.swift
@@ -20,23 +20,29 @@ extension EditView {
         var description: String
         var loadingState = LoadingState.loading
         var pages = [Page]()
-    }
-    
-    func fetchNearbyPlaces() async {
-        let urlString = "https://en.wikipedia.org/w/api.php?ggscoord=\(location.latitude)%7C\(location.longitude)&action=query&prop=coordinates%7Cpageimages%7Cpageterms&colimit=50&piprop=thumbnail&pithumbsize=500&pilimit=50&wbptterms=description&generator=geosearch&ggsradius=10000&ggslimit=50&format=json"
         
-        guard let url = URL(string: urlString) else {
-            print("Bad URL: \(urlString)")
-            return
+        init(location: Location) {
+            name = location.name
+            description = location.description
+            self.location = location
         }
         
-        do {
-            let (data, _) = try await URLSession.shared.data(from: url) // fetch
-            let items = try JSONDecoder().decode(Result.self, from: data) // decode
-            pages = items.query.pages.values.sorted()
-            loadingState = .loaded
-        } catch {
-            loadingState = .failed
+        func fetchNearbyPlaces() async {
+            let urlString = "https://en.wikipedia.org/w/api.php?ggscoord=\(location.latitude)%7C\(location.longitude)&action=query&prop=coordinates%7Cpageimages%7Cpageterms&colimit=50&piprop=thumbnail&pithumbsize=500&pilimit=50&wbptterms=description&generator=geosearch&ggsradius=10000&ggslimit=50&format=json"
+            
+            guard let url = URL(string: urlString) else {
+                print("Bad URL: \(urlString)")
+                return
+            }
+            
+            do {
+                let (data, _) = try await URLSession.shared.data(from: url) // fetch
+                let items = try JSONDecoder().decode(Result.self, from: data) // decode
+                pages = items.query.pages.values.sorted()
+                loadingState = .loaded
+            } catch {
+                loadingState = .failed
+            }
         }
     }
 }
